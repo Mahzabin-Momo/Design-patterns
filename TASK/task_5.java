8f7410377e9eba473c71e4044f3f5230b8052f24
Mahzabin-Momo <139198649+Mahzabin-Momo@users.noreply.github.com>
2023-10-12 19:09:45 +0600

    Add files via upload

---
+++ TASK/task_5.java
@@ -0,0 +1,43 @@
+interface Message {
+    void Notify();
+}
+
+class Notification {
+    private Message notification;
+
+    public Notification(Message notification) {
+        this.notification = notification;
+    }
+
+    public void Notify() {
+        notification.Notify();
+    }
+}
+
+class MsgNotification implements Message {
+    @Override
+    public void Notify() {
+        System.out.println("Msg Notification");
+    }
+}
+
+class CallNotification implements Message {
+    @Override
+    public void Notify() {
+        System.out.println("Call Notification");
+    }
+}
+
+class MainClassTest {
+    public static void main(String[] args) {
+        MsgNotification msgNotification = new MsgNotification();
+        CallNotification callNotification = new CallNotification();
+
+        Notification msgNotify = new Notification(msgNotification);
+        Notification callNotify = new Notification(callNotification);
+
+        msgNotify.Notify();
+        System.out.println("");
+        callNotify.Notify();
+    }
+}
